a20b9e94411f16b42ccb54695982f10019c31317
hana <zinc0214@gmail.com>
2021-11-08 23:31:59 +0900

    7. Create your custom layout

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/zinc/layoutcomposecodelab/compose/BaseLine.kt
@@ -0,0 +1,89 @@
+package com.zinc.layoutcomposecodelab.compose
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.zinc.layoutcomposecodelab.ui.theme.LayoutComposeCodeLabTheme
+
+fun Modifier.firstBaselineToTop(
+    firstBaseLineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        val placeableY = firstBaseLineToTop.roundToPx() - firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BaseLineBodyContent(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutComposeCodeLabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutComposeCodeLabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun BaseLineBodyContentPreview() {
+    LayoutComposeCodeLabTheme {
+        BaseLineBodyContent()
+    }
+}
