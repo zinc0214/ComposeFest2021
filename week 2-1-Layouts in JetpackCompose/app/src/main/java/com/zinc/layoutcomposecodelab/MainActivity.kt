335662557cb6ffb3ccc1a33a459f190a7afd5414
hana <zinc0214@gmail.com>
2021-11-08 22:22:42 +0900

    3. Modifiers

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/zinc/layoutcomposecodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/zinc/layoutcomposecodelab/MainActivity.kt
@@ -3,11 +3,23 @@ package com.zinc.layoutcomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zinc.layoutcomposecodelab.ui.theme.LayoutComposeCodeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +29,10 @@ class MainActivity : ComponentActivity() {
             LayoutComposeCodeLabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    // Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LayoutComposeCodeLabTheme {
-        Greeting("Android")
-    }
-}
